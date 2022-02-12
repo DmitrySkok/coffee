a29351810c92e2d9348376697a5869898505e490
Dmitry Skok <dimas.skok95@gmail.com>
2022-02-12 14:03:55 +0100

    Add scroll to block

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,39 @@
+import { settings } from './settings.js';
 const app = {
-  init: function() {
+  initData: function () {
+    const url = settings.db.url + '/' + settings.db.products;
+    this.data = {};
+    fetch(url)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((parsedResponse) => {
+        this.data.products = parsedResponse;
+      });
   },
+
+  init: function () {
+    const thisApp = this;
+
+    thisApp.initData();
+    thisApp.scrollTo();
+  },
+
+  scrollTo: function () {
+    const anchors = document.querySelectorAll('a.scroll-to');
+
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href');
+
+        document.querySelector(blockID).scrollIntoView({
+          block: 'start'
+        });
+      });
+    }
+  }
 };
 
 app.init();
