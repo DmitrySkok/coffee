7bf8a7443b562414c64d266e68d0327a8abc3df9
Dmitry Skok <dimas.skok95@gmail.com>
2022-02-14 18:01:53 +0100

    Add animated arrow

---
+++ src/js/components/products.js
@@ -0,0 +1,23 @@
+/* class Products {
+  constructor() {
+    const thisProducts = this;
+
+    thisProducts.initData();
+    thisProducts.getElements();
+    thisProducts.render();
+  }
+  initData() {
+    const thisProducts = this;
+
+  }
+
+  getElements() {
+    const thisProducts = this;
+
+  }
+
+  render() {
+    const thisProducts = this;
+
+  }
+} */
